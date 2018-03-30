ef18d480cfca0bb9eb334ee67b395d4bb541ab9c
Miguel de Braganca <miguelofboston@gmail.com>
2018-03-30 19:45:11 +0100

    built basic rails CRUD for restaurant listing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(name: 'Terra', address: 'Principe Real', rating: 4)
+Restaurant.create(name: 'Loucos de Lisboa', address: 'Bairro Alto', rating: 5)
+Restaurant.create(name: 'Dishoom', address: "Shoreditch", rating: 3)
